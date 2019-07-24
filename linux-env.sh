5f8fb25b0b08c3366ceb9c24e96cdc0e24f60c84
saziknows <52761798+saziknows@users.noreply.github.com>
2019-07-24 18:12:19 +0200

    Add files via upload

---
+++ linux-env.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo '
+  _____.__                 __                                 
+_/ ____\  | _____    _____|  | __           ____   _______  __
+\   __\|  | \__  \  /  ___/  |/ /  ______ _/ __ \ /    \  \/ /
+ |  |  |  |__/ __ \_\___ \|    <  /_____/ \  ___/|   |  \   / 
+ |__|  |____(____  /____  >__|_ \          \___  >___|  /\_/  
+                 \/     \/     \/              \/     \/      
+' 
+
+
+apt-get install python3
+apt-get install pip3
+
+#mkdir 'Flask-env'
+
+#cd Flask-env
+
+#pip3 install virtualenv
+
+#virtualenv env
+
+#source env/bin/activate
+
+#pip3 install flask flask-sqlalchemy 
